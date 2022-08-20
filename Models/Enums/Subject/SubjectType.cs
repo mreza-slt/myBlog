83390105b40a6bf8107d5f27b9accd755191fa66
m-reza <rsltany649@gmail.com>
2022-08-20 14:38:58 +0430

    Create Subject Api

---
+++ Models/Enums/Subject/SubjectType.cs
@@ -0,0 +1,11 @@
+﻿namespace MyBlog.Models.Enums.Subject
+{
+    /// <summary>
+    /// نوع دسته بندی / موضوع
+    /// </summary>
+    public enum SubjectType : int
+    {
+        MajorSubject = 1,
+        ForumSubject = 2,
+    }
+}
---
+++ Models/ViewModels/Subject/SubjectViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using MyBlog.Models.Enums.Subject;
+
+namespace MyBlog.Models.ViewModels.Subject
+{
+    /// <summary>
+    /// اضافه کردن موضوع فقط توسط ادمین وبسایت
+    /// </summary>
+    public class SubjectViewModel
+    {
+        [Required(ErrorMessage = "نام را وارد کنید")]
+        public string Name { get; set; } = null!;
+
+        /// <summary>
+        ///  نوع دسته بندی
+        /// </summary>
+        [EnumDataType(typeof(SubjectType), ErrorMessage = "نوع دسته بندی را انتخاب کنید")]
+        [Required(ErrorMessage = "مقدار نوع دسته بندی را وارد کنید")]
+        public SubjectType SubjectType { get; set; }
+
+        /// <summary>
+        /// شناسه والد
+        /// </summary>
+        public long? ParentId { get; set; }
+    }
+}
