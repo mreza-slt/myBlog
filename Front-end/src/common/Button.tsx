89840d53343b980172e534f13b79e7ba4c4f11fe
m-reza <rsltany649@gmail.com>
2022-09-21 14:21:52 +0430

    fix color button in classNames

--- Front-end/src/common/Button.tsx
+++ Front-end/src/common/Button.tsx
@@ -17,7 +17,15 @@ export default function Button({
       type={type}
       className={classNames(
         { "opacity-70": disabled },
-        `flex cursor-pointer justify-center w-full items-center transition-all duration-300 text-white hover:scale-[.96] border border-${color}-700 bg-${color}-800 hover:ring-4 hover:outline-none hover:ring-${color}-300 focus:ring-4 focus:outline-none focus:ring-${color}-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:border-${color}-500 dark:text-${color}-500 dark:hover:text-white dark:hover:bg-${color}-600 dark:focus:ring-${color}-900`
+        {
+          "border-violet-700 bg-violet-800 hover:ring-violet-300 focus:ring-violet-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:border-violet-500 dark:text-violet-500  dark:hover:bg-violet-600 dark:focus:ring-violet-900":
+            color === "violet",
+        },
+        {
+          "border-red-700 bg-red-800 hover:ring-red-300 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:border-red-500 dark:text-red-500  dark:hover:bg-red-600 dark:focus:ring-red-900":
+            color === "red",
+        },
+        `flex cursor-pointer justify-center w-full items-center transition-all duration-300 text-white hover:scale-[.96] border hover:ring-4 hover:outline-none focus:ring-4 focus:outline-none dark:hover:text-white`
       )}
     >
       {children}
